4154d689ca6cd2a81e62f8745981bfa6bea3468b
Dhiraj <iamdhirajkrprasad@gmail.com>
2023-10-21 12:19:56 +0530

    working on the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import "../css/Login.css";
+// import {useNavigate} from "react-router";
+//import "./backend/index";
+// import { useHistory } from "react-router";
 
 function Login() {
-  return <div className="page-container"> Login Page</div>;
+  // const history=useNavigate();
+  const [name, setName] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  async function submit(e) {
+    e.preventDefault();
+
+    try {
+      await axios
+        .post("http://localhost:3500/api/users", {
+          name,
+          password,
+        })
+        .then((res) => {
+          if (res.data === "exist") {
+            alert("Login successfully");
+            localStorage.setItem("token", res.data.token);
+            // history('/')
+          } else if (res.data === "not exist") {
+            alert("Email id or password is not valid");
+          }
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <div className="login-form">
+        <div className="title">LOGIN</div>
+        <div className="form">
+          <form action="/test" method="post">
+            <div className="input-container">
+              <label id="uname">Username</label>
+              <input
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                className="input"
+                placeholder="name"
+                type="text"
+              />
+            </div>
+            <div className="input-container">
+              <label id="uname">Password</label>
+              <input
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className="input"
+                placeholder="password"
+                type="password"
+              />
+            </div>
+            <div className="button-container">
+              <input type="submit" onClick={submit} />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
